2ff4bb103bb895af7a5977807688366e22f2a0d7
SJ Hong <esjay12321>
2018-04-21 23:01:28 +0800

    Added assets of statistics page

---
+++ prototype1/prototype1/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  prototype1
+//
+//  Created by Esjay Hong on 21/04/2018.
+//  Copyright © 2018 Minkyung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Profile"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
