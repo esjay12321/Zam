eb0ada8268cb9f949419bb9fd2f386c98e0eb8c1
chloeminkyung <chloeminkyung@gmail.com>
2018-04-21 20:28:35 +0800

    Add files via upload

---
+++ prototype1/prototype1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prototype1
+//
+//  Created by Minkyung Kim on 21/04/2018.
+//  Copyright © 2018 Minkyung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
