410d29f3d8203dab23901e650750e8d65dba3112
SJ Hong <esjay12321>
2018-04-22 11:43:39 +0800

    Implemented Messages

---
+++ prototype1/prototype1/MessagesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MessagesViewController.swift
+//  prototype1
+//
+//  Created by Esjay Hong on 22/04/2018.
+//  Copyright © 2018 Minkyung Kim. All rights reserved.
+//
+
+import UIKit
+
+class MessagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
